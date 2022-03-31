99e40d882ce9289a667910d5ced6af5bc79bad85
lahlaliwassim2 <wlahlali34@gmail.com>
2022-03-31 00:10:24 +0000

    appliquer le principe de dry

--- index.php
+++ index.php
@@ -2,14 +2,9 @@
 include 'composants/header.php';
 ?>
 <body>
-<nav class="navbar navbar-light  bg-secondary">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">Contact liste</a>
-    <form > 
-  <button class="btn  " type="submit">login</button>
-   </form> 
-  </div>
-</nav>
+<?php
+include'composants/navbar.php'
+?>
 
 <div class="container h-50   g-start-2 row martop color-danger m-auto">
   <div class="w-50 mx-10 ">
@@ -18,6 +13,4 @@ include 'composants/header.php';
   <p class="fs-5">Already have an accont? <span class="text-primary fs-4"> Login here.</span></p>
 </div>
 </div>
-
-
 </body>
