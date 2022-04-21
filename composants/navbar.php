b3741db6bb6d2b2c44c58ec0ecaa9b43f9d70190
lahlaliwassim2 <wlahlali34@gmail.com>
2022-04-21 10:19:36 +0000

    initialisation

--- composants/navbar.php
+++
@@ -1,8 +0,0 @@
-<nav class="navbar navbar-light  bg-secondary">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="../login.php">Contact liste</a>
-    <form > 
-  <button class="btn  " type="submit">login</button>
-   </form> 
-  </div>
-</nav>
